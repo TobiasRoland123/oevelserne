9305d3e60d49d065dfbd16113ed024b0591f9cb5
Tobias Roland <samu1493@stud.kea.dk>
2022-08-22 12:39:06 +0200

    løst

---
+++ oevelse_3/script.js
@@ -0,0 +1,21 @@
+let ranTal = Math.round(Math.random() * 11);
+let forsoeg = 0;
+
+let chekKnap = document.querySelector(".checker");
+let gaet;
+
+chekKnap.addEventListener("click", checkTal);
+
+function checkTal() {
+  gaet = document.querySelector("#tal").value;
+  console.log(gaet);
+
+  forsoeg++;
+  if (gaet == ranTal) {
+    alert(ranTal + " er rigtigt og du har brugt " + forsoeg + " forsøg. Prøv med et nyt tal :)");
+  } else if (gaet > ranTal) {
+    console.log("Øv! " + gaet + " var for højt. Prøv igen :)");
+  } else {
+    console.log("Øv! " + gaet + " var for lavt. Prøv igen :)");
+  }
+}
