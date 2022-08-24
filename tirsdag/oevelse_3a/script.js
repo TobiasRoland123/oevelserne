23274076e751919d469bcca5d153f78751588a63
Tobias Roland <samu1493@stud.kea.dk>
2022-08-24 21:18:39 +0200

    opgave 3 løst

---
+++ tirsdag/oevelse_3a/script.js
@@ -0,0 +1,25 @@
+const titeloverskrift = document.querySelector("header");
+const underoverskrift = document.querySelector("main");
+const article1p = document.querySelector("article:nth-child(1) p");
+const article1h3 = document.querySelector("article:nth-child(1) h3");
+const pic1 = document.querySelector("article:nth-child(1) img");
+const pic2 = document.querySelector("article:nth-child(2) img");
+const article2h3 = document.querySelector("article:nth-child(2) h3");
+const article2p = document.querySelector("article:nth-child(2) p");
+const parag = document.querySelector("p");
+const footer = document.querySelector("footer");
+
+titeloverskrift.textContent = "Overskrift";
+
+article1p.textContent = "Dette er en lorteartikel om at være mega lort";
+
+article1h3.textContent = "lorteartikel";
+
+article2h3.innerHTML = "<h3> Dette er den fede artikel</h3>";
+article2p.innerHTML = "<p> Det her er en artikel, som handler om at være top nice.</p>";
+
+console.log(pic1);
+
+pic1.src = "http://placeimg.com/640/480/any";
+
+pic2.src = "billeder/random.jpg";
