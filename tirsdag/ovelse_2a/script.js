135b5749e87955d0cc47d7d337b300010b9d7375
Tobias Roland <samu1493@stud.kea.dk>
2022-08-24 20:19:02 +0200

    opgave 2 løst

---
+++ tirsdag/ovelse_2a/script.js
@@ -0,0 +1,7 @@
+const titeloverskrift = document.querySelector("header");
+const underoverskrift = document.querySelector("main");
+const articles = document.querySelector("article");
+const article = document.querySelector("article");
+const footer = document.querySelector("footer");
+
+console.log(titeloverskrift.textContent);
